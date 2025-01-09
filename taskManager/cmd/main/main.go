7e02f7bea80aff15c07f4021c470c4ee7c1cba3b
Adejare <adejare.arasheed@gmail.com>
2025-01-09 22:25:58 +0100

    Main

---
+++ taskManager/cmd/main/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Adejare77/go/taskManager/config"
+	"github.com/Adejare77/go/taskManager/internals/middlewares"
+	"github.com/Adejare77/go/taskManager/internals/routes"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	app := gin.Default()
+
+	app.Use(sessions.Sessions("taskManager", config.SessionStore))
+
+	// Public Routes
+	publicRoutes := app.Group("/")
+	routes.PublicRoutes(publicRoutes)
+
+	// Protected Routes
+	protectedRoutes := app.Group("/", middlewares.AuthMiddleware())
+	routes.ProtectedRoutes(protectedRoutes)
+
+	fmt.Println("Running Server on Port 3000")
+	if err := app.Run(":3000"); err != nil {
+		log.Fatal("Could not Start the Server")
+	}
+}
+
+// Handle 404 Not Found
+// router.NoRoute(func(ctx *gin.Context) {
+// 	ctx.JSON(http.StatusNotFound, gin.H{
+// 		"error":   "Page not found",
+// 		"message": "The requested URL was not found on this server.",
+// 	})
+// })
