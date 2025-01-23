efb4aa7945796404b8e408630ce8811fc9b557df
Adejare <adejare.arasheed@gmail.com>
2025-01-23 17:39:40 +0100

    User Table Database

--- taskManager/internals/models/usersDB.go
+++ taskManager/internals/models/usersDB.go
@@ -2,43 +2,30 @@ package models
 
 import (
 	"errors"
-	"time"
 
 	"github.com/Adejare77/go/taskManager/config"
+	"github.com/Adejare77/go/taskManager/internals/schemas"
 	"gorm.io/gorm"
 )
 
-type User struct {
-	ID        uint `gorm:"primarykey"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	FullName  string `json:"fullname" gorm:"column:fullName" binding:"required"`
-	Email     string `json:"email" gorm:"column:email" binding:"required"`
-	Password  string `json:"password" gorm:"column:password" binding:"required"`
-	Tasks     []Task `json:"-" gorm:"column:tasks"`
-}
-
 var db = config.DB
 
-func GetTasksByUserID(userID uint) ([]Task, error) {
-	var user User
-	if err := db.Preload("Tasks").First(&user, userID).Error; err != nil {
-		return nil, err
-	}
-	return user.Tasks, nil
-}
-
-func GetTasksByStatus(userID uint, status string) ([]Task, error) {
-	var user User
+func GetTasksByStatus(userID uint, status string) ([]schemas.TaskOutput, error) {
+	var user schemas.User
 	if err := db.Preload("Tasks", "status = ?", status).First(&user, userID).Error; err != nil {
 		return nil, err
 	}
-	return user.Tasks, nil
+
+	var taskOutput []schemas.TaskOutput
+	for _, task := range user.Tasks {
+		taskOutput = append(taskOutput, schemas.ToTaskOutput(task))
+	}
+	return taskOutput, nil
 }
 
 func GetInfo(email string) (uint, string, error) {
-	var user User
-	err := db.Where("email=?", email).First(&user).Error
+	var user schemas.User
+	err := db.Debug().Where("email=?", email).First(&user).Error
 
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return 0, "", err
@@ -46,7 +33,7 @@ func GetInfo(email string) (uint, string, error) {
 	return user.ID, user.Password, nil
 }
 
-func (user *User) Create() error {
+func Create(user schemas.User) error {
 	err := db.Create(&user).Error
 
 	if err != nil {
@@ -56,8 +43,7 @@ func (user *User) Create() error {
 }
 
 func DeleteUser(userID string) error {
-	// db.Unscoped().Delete()  // for hard delete
-	if err := db.Unscoped().Delete(&User{}, userID).Error; err != nil {
+	if err := db.Unscoped().Delete(&schemas.User{}, userID).Error; err != nil {
 		return err
 	}
 	return nil
