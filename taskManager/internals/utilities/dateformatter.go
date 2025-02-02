bf2460d28f38b15032ef4209fdff7501ca985a80
Adejare <adejare.arasheed@gmail.com>
2025-02-02 07:38:48 +0100

    startDate and dueDate format

--- taskManager/internals/utilities/dateformatter.go
+++ taskManager/internals/utilities/dateformatter.go
@@ -1,14 +1,34 @@
 package utilities
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 	"time"
 )
 
-// func TimeManipulator(input string) time.Time {
-func TimeManipulator(input string) time.Time {
-	layout := "2006-01-02T15:04" // Constant layout
+func StartTimeManipulator(input string) (time.Time, error) {
+	if input == "" {
+		return time.Now(), nil
+	}
+
+	layout := "2006-01-02 15:04" // Constant layout
+	parsedTime, err := time.Parse(layout, input)
+	if err != nil {
+		fmt.Println("DATEFORMATTER", input) // Meant to check where the error is
+		fmt.Println("Error: ", err)         // Takes only the required format
+		return time.Time{}, err
+	}
+
+	if parsedTime.Before(time.Now()) {
+		return time.Time{}, errors.New("start date cannot be in the past")
+	}
+
+	return parsedTime, nil
+}
+
+func DueTimeManipulator(input string, startDate time.Time) (time.Time, error) {
+	layout := "2006-01-02 15:04" // Constant layout
 
 	if strings.Contains(input, "day") || strings.Contains(input, "hour") || strings.Contains(input, "minute") {
 		var number int
@@ -16,28 +36,21 @@ func TimeManipulator(input string) time.Time {
 
 		fmt.Sscanf(input, "%d %s", &number, &unit)
 
-		currentTime := time.Now()
-
-		// switch {
-		// case strings.Contains(input, "day"):
-		// 	return (currentTime.Add(time.Duration(number) * 24 * time.Hour)).Format(layout)
-		// case strings.Contains(input, "minutes"):
-		// 	return (currentTime.Add(time.Duration(number) * time.Minute)).Format(layout)
-		// case strings.Contains(input, "hour"):
-		// 	return (currentTime.Add(time.Duration(number) * time.Hour)).Format(layout)
-		// }
-
 		switch {
 		case strings.Contains(input, "day"):
-			return currentTime.Add(time.Duration(number) * 24 * time.Hour)
+			return startDate.Add(time.Duration(number) * 24 * time.Hour), nil
 		case strings.Contains(input, "minutes"):
-			return currentTime.Add(time.Duration(number) * time.Minute)
+			return startDate.Add(time.Duration(number) * time.Minute), nil
 		case strings.Contains(input, "hour"):
-			return currentTime.Add(time.Duration(number) * time.Hour)
+			return startDate.Add(time.Duration(number) * time.Hour), nil
 		}
 	}
 
 	parsedTime, _ := time.Parse(layout, input)
-	// return parsedTime.Format(layout)
-	return parsedTime
+
+	if parsedTime.Before(startDate) {
+		return time.Time{}, errors.New("due date must be After start date")
+	}
+
+	return parsedTime, nil
 }
