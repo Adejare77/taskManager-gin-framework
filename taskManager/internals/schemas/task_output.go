af6593abfe4be3aa11c92ca1d98aad23308ccfc7
Adejare <adejare.arasheed@gmail.com>
2025-02-02 07:48:35 +0100

    Convert Incoming fields 'Task' struct and 'Task' fields to response 'TaskOutput' struct

--- taskManager/internals/schemas/task_output.go
+++ taskManager/internals/schemas/task_output.go
@@ -1,16 +1,14 @@
 package schemas
 
+import "github.com/Adejare77/go/taskManager/internals/utilities"
+
 type TaskOutput struct {
 	TaskID    string `gorm:"column:taskID"`
-	Desc      string `json:"description" binding:"required"`
-	Title     string `json:"title" binding:"required"`
-	StartDate string `json:"startDate" binding:"date"` // Default to time.Now()
-	DueDate   string `json:"dueDate" binding:"required,date"`
+	Desc      string `json:"description"`
+	Title     string `json:"title"`
+	StartDate string `json:"startDate" binding:"startdate"` // Default to time.Now()
+	DueDate   string `json:"dueDate" binding:"duedate"`
 	Status    string `json:"status"`
-}
-
-func (task *TaskOutput) TableName() string {
-	return "tasks"
 }
 
 func ToTaskOutput(task Task) TaskOutput {
@@ -22,4 +20,36 @@ func ToTaskOutput(task Task) TaskOutput {
 		DueDate:   task.DueDate.Format("2006-01-02 15:04:05"),
 		Status:    task.Status,
 	}
+}
+
+type PostTask struct {
+	Desc      string `json:"description" binding:"required"`
+	Title     string `json:"title" binding:"required"`
+	StartDate string `json:"startDate" binding:"startdate"` // Default to time.Now()
+	DueDate   string `json:"dueDate" binding:"required,duedate"`
+	Status    string `json:"status"`
+}
+
+type DateTimeUpdate struct {
+	StartDate string `json:"startDate" binding:"startdate"`
+	DueDate   string `json:"dueDate"`
+}
+
+func ToTask(task PostTask) (Task, error) {
+	startDate, err := utilities.StartTimeManipulator(task.StartDate)
+	if err != nil {
+		return Task{}, err
+	}
+
+	dueDate, err := utilities.DueTimeManipulator(task.DueDate, startDate)
+	if err != nil {
+		return Task{}, err
+	}
+	return Task{
+		Desc:      task.Desc,
+		Title:     task.Title,
+		StartDate: startDate,
+		DueDate:   dueDate,
+		Status:    task.Status,
+	}, nil
 }
