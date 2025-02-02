5324936841b9fe767e4f638102de3b9406127f4a
Adejare <adejare.arasheed@gmail.com>
2025-02-02 07:46:00 +0100

    Users Table schema

--- taskManager/internals/schemas/users.go
+++ taskManager/internals/schemas/users.go
@@ -11,9 +11,9 @@ type User struct {
 	ID        uint `gorm:"primaryKey"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	FullName  string `gorm:"column:fullName;not null"`
-	Email     string `gorm:"column:email;unique;not null"`
-	Password  string `gorm:"column:password;not null"`
+	FullName  string `gorm:"column:fullName;not null" binding:"required"`
+	Email     string `gorm:"column:email;unique;not null" binding:"required,email"`
+	Password  string `gorm:"column:password;not null" binding:"required"`
 	Tasks     []Task `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;"`
 }
 
