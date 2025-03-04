78928763ca5caf3a839e7e9fc3b29218c9121d9d
Adejare <adejare.arasheed@gmail.com>
2025-03-04 12:43:59 +0100

    Error Handlers

--- taskManager/internals/handlers/errors.go
+++ taskManager/internals/handlers/errors.go
@@ -4,53 +4,56 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
-func BadRequest(ctx *gin.Context) {
-	ctx.JSON(http.StatusBadRequest, gin.H{
-		"error": "Bad Request",
+type APIError struct {
+	StatusCode int         `json:"-"`
+	ErrorCode  string      `json:"error_code"`
+	Message    string      `json:"message"`
+	Details    interface{} `json:"details,omitempty"`
+}
+
+func HandleError(ctx *gin.Context, statusCode int, errorCode string, message string, reason string, details interface{}) {
+	// For logging
+	logrus.WithFields(logrus.Fields{
+		"user_id":     ctx.MustGet("userID"),
+		"task_id":     ctx.Param("taskID"),
+		"status_code": statusCode,
+		"error_code":  errorCode,
+		"details":     details,
+	}).Error(reason)
+
+	// For response to user
+	ctx.JSON(statusCode, APIError{
+		StatusCode: statusCode,
+		ErrorCode:  errorCode,
+		Message:    message,
+		// Details:    details,
 	})
 }
 
-func BadRequestWithMsg(ctx *gin.Context, msg interface{}) {
-	ctx.JSON(http.StatusBadRequest, gin.H{
-		"error": "Bad Request",
-		"msg":   msg,
-	})
+func BadRequest(ctx *gin.Context, reason string, details interface{}) {
+	HandleError(ctx, http.StatusBadRequest, "BAD_REQUEST", "Invalid request", reason, details)
 }
 
-func InternalServerError(ctx *gin.Context) {
-	ctx.JSON(http.StatusInternalServerError, gin.H{
-		"error": "Internal Server Error",
-	})
+func Unauthorized(ctx *gin.Context, reason string, details interface{}) {
+	HandleError(ctx, http.StatusUnauthorized, "UNAUTHORIZED", "Unauthorized", reason, details)
 }
 
-func InternalServerErrorWithMsg(ctx *gin.Context, msg string) {
-	ctx.JSON(http.StatusInternalServerError, gin.H{
-		"error": "Internal Server Error",
-		"msg":   msg,
-	})
+func InternalServerError(ctx *gin.Context, reason string, details interface{}) {
+	HandleError(ctx, http.StatusInternalServerError, "INTERNAL_SERVER_ERROR", "An internal server error occured", reason, nil)
 }
 
-func Unauthorized(ctx *gin.Context) {
-	ctx.JSON(http.StatusUnauthorized, gin.H{
-		"error": "Unauthorized",
-	})
-}
-
-func UnauthorizedWithMsg(ctx *gin.Context, msg string) {
-	ctx.JSON(http.StatusUnauthorized, gin.H{
-		"error": "Unauthorized",
-		"msg":   msg,
-	})
+func NotFound(ctx *gin.Context, reason string, details interface{}) {
+	HandleError(ctx, http.StatusOK, "NOT_FOUND", "Task not found", reason, details)
 }
 
 func PageNotFound(ctx *gin.Context) {
-	ctx.JSON(http.StatusNotFound, gin.H{
-		"error": "Page Not Found",
-		"Available routes": []string{
+	HandleError(ctx, http.StatusNotFound, "PAGE_NOT_FOUND", "Page not found", "Invalid Route", gin.H{
+		"available_routes": []string{
 			"/register",
-			"/loging",
+			"/login",
 			"/tasks",
 			"/task/:id",
 		},
