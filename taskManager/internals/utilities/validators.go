9100232ed0f3a1ebef9f1f00664a39c725f07a79
Rashisky <rashisky007@gmail.com>
2025-04-17 19:51:54 +0100

    Include CompareDates function to compare start and due dates. Remove custom date validation

--- taskManager/internals/utilities/validators.go
+++ taskManager/internals/utilities/validators.go
@@ -5,107 +5,52 @@ import (
 	"strings"
 	"time"
 
-	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 )
 
-func startDateValidation(fl validator.FieldLevel) bool {
-	startDate := fl.Field().String()
+var layout = "2006-01-02 15:04"
 
-	layout := "2006-01-02 15:04" // Constant layout
+func CompareDates(startDate *JSONTime, dueDate JSONTime) (JSONTime, error) {
+	var startTime time.Time
+	dueTime := time.Time(dueDate)
+	currentTime := time.Now()
 
-	_, err := time.Parse(layout, startDate)
-	if err != nil {
-		return err == nil
-	}
-	return true
-}
+	if startDate == nil {
+		startTime = currentTime
 
-func dueDateValidation(fl validator.FieldLevel) bool {
-	date := fl.Field().String()
-
-	if strings.Contains(date, "day") || strings.Contains(date, "hour") || strings.Contains(date, "minute") {
-		var number int
-		var unit string
-
-		_, err := fmt.Sscanf(date, "%d %s", &number, &unit)
-		if err != nil {
-			return false
-		}
-
-		switch {
-		case strings.Contains(date, "day"):
-			return true
-		case strings.Contains(date, "minute"):
-			return true
-		case strings.Contains(date, "hour"):
-			return true
-		default:
-			return false
+	} else {
+		startTime = time.Time(*startDate)
+		if startTime.Before(currentTime) {
+			return JSONTime{}, fmt.Errorf("start_date (%s) cannot be in past of current time (%s)", startTime, currentTime)
 		}
 	}
 
-	layout := "2006-01-02 15:04" // Constant layout
-
-	_, err := time.Parse(layout, date)
-	if err != nil {
-		return err == nil
+	if dueTime.Before(startTime) {
+		return JSONTime{}, fmt.Errorf("due_date (%s) cannot be in past of start_date (%s)", dueTime, startTime)
 	}
 
-	return true
+	return JSONTime(startTime), nil
 }
 
-func RegisterValidation() {
-	// Register the above Validations
-	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		v.RegisterValidation("startdate", startDateValidation)
-		v.RegisterValidation("duedate", dueDateValidation)
-	}
-}
+func ValidationError(err error) []string {
+	var errorDetails []string
+	fieldErrors, _ := err.(validator.ValidationErrors)
+	validationErr := "validation failed:"
 
-// func ValidationError(err validator.ValidationErrors) []any {
-// 	var errorDetails []any
-// 	for _, fieldError := range err {
-// 		if fieldError.Tag() == "required" {
-// 			errorDetails = append(errorDetails, fmt.Sprintf(
-// 				"missing %s field", fieldError.Field(),
-// 			))
-// 		} else if fieldError.Tag() == "duedate" {
-// 			errorDetails = append(errorDetails,
-// 				map[string]any{
-// 					"dueDate format": "`YYYY-MM-DD HH:MM` e.g., 2024-05-19 22:15," +
-// 						"`x day(s)` e.g., 3 days (relative to the current time)," +
-// 						"`x hour(s)` e.g., 5 hours (relative to the current time)",
-// 				},
-// 			)
-// 		} else if fieldError.Tag() == "startdate" {
-// 			errorDetails = append(errorDetails,
-// 				"startDate format: `YYYY-MM-DD HH:MM` e.g., 2024-05-19 22:15",
-// 			)
-// 		} else if fieldError.Tag() == "email" {
-// 			errorDetails = append(errorDetails, "Invalid email Format")
-// 		} else {
-// 			errorDetails = append(errorDetails, err.Error())
-// 		}
-// 	}
-// 	return errorDetails
-// }
-
-func ValidationError(err error) interface{} {
-	if err == nil {
-		return nil
-	}
-
-	// Handle Validation errors
-	if fieldErrors, ok := err.(validator.ValidationErrors); ok {
-		errorMessages := make(map[string]string)
-		for _, fieldError := range fieldErrors {
-			field := strings.ToLower(fieldError.Field())
-			errorMessages[field] = fmt.Sprintf("Invalid value for %s: %s", field, fieldError.Tag())
+	for _, fieldError := range fieldErrors {
+		field := strings.ToLower(fieldError.Field())
+		if fieldError.Tag() == "required" {
+			errorDetails = append(errorDetails, fmt.Sprintf("%s missing %s field", validationErr, field))
+		} else if fieldError.Tag() == "numeric" {
+			errorDetails = append(errorDetails, fmt.Sprintf("%s %s can only be a numeric value", validationErr, field))
+		} else if fieldError.Tag() == "min" {
+			errorDetails = append(errorDetails, fmt.Sprintf("%s %s cannot be less than 1", validationErr, field))
+		} else if fieldError.Tag() == "oneof" {
+			errorDetails = append(errorDetails, fmt.Sprintf("%s `%s` can only be one of this fields: pending, in-progress, completed", validationErr, field))
+		} else {
+			errorDetails = append(errorDetails, fmt.Sprintf("%s unresolved validation", validationErr))
 		}
-		return errorMessages
 	}
+	return errorDetails
 
-	// Handle other types of errors (e.g JSON parsing errors)
-	return err.Error()
 }
