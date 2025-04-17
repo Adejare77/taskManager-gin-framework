1bd77e9783798b10c66b3a74fcb1479b44c8d4ee
Rashisky <rashisky007@gmail.com>
2025-04-17 19:56:00 +0100

    Improve environment variable to use more defaults

--- taskManager/config/config.go
+++ taskManager/config/config.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/Adejare77/go/taskManager/internals/handlers"
 	"github.com/Adejare77/go/taskManager/internals/schemas"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -25,29 +26,32 @@ func Connect() error {
 	// Load database Configuration
 	cfg, err := loadDBConfig()
 	if err != nil {
-		return fmt.Errorf("(DB Configuration) %v", err)
+		return fmt.Errorf("DB Configuration Error %v", err)
 	}
 
 	// Create Data Source Name (DSN)
-	dsn := fmt.Sprintf("user=%s password=%s host=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Taipei",
+	dsn := fmt.Sprintf("user=%s password=%s host=%s dbname=%s port=5432 sslmode=disable TimeZone=Africa/Lagos",
 		cfg.User, cfg.Password, cfg.Host, cfg.DBName)
 
 	// Open Database Connection
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		fmt.Println("***ERROR DETECTED")
-		return fmt.Errorf("(Connection) %v", err)
+		return fmt.Errorf("failed to connect to database %v", err)
 	}
 
 	// Configure connection pool
 	sqlDB, err := db.DB()
 	if err != nil {
-		return fmt.Errorf("(Connection Instance) %v", err)
+		return fmt.Errorf("connection Instance %v", err)
 	}
 
 	maxOpenConns, _ := strconv.Atoi(os.Getenv("DB_MAX_OPEN_CONNS"))
 	maxIdleConns, _ := strconv.Atoi(os.Getenv("DB_MAX_IDLE_CONNS"))
-	connMaxLifetime, _ := time.ParseDuration(os.Getenv("DB_CONN_MAX_LIFETIME"))
+	connMaxLifetime, err := time.ParseDuration(os.Getenv("DB_CONN_MAX_LIFETIME"))
+	if err != nil {
+		connMaxLifetime = time.Duration(30)
+		handlers.Warning("invalid DB_CONN_MAX_LIFETIME. Defaults to 30 minutes")
+	}
 
 	sqlDB.SetMaxOpenConns(maxOpenConns)
 	sqlDB.SetConnMaxIdleTime(time.Duration(maxIdleConns))
@@ -55,13 +59,10 @@ func Connect() error {
 
 	// Auto Migrate tables into the Database
 	if err := db.AutoMigrate(&schemas.User{}, &schemas.Task{}); err != nil {
-		return fmt.Errorf("(Auto-Migration) %v", err)
+		return fmt.Errorf("Auto-Migration Error %v", err)
 	}
 
 	DB = db
-	fmt.Println("----------- db connected -------------")
-	fmt.Println(DB)
-	fmt.Println("----------- db connected -------------")
 	return nil
 }
 
@@ -69,11 +70,19 @@ func Connect() error {
 func loadDBConfig() (*DBConfig, error) {
 	port, err := strconv.Atoi(os.Getenv("DB_PORT"))
 	if err != nil {
-		return nil, fmt.Errorf("(DB_PORT) %v", err)
+		port = 5432
+		handlers.Warning("invalid DB_PORT. Defaults to 5432")
+		// return nil, fmt.Errorf("(DB_PORT) %v", err)
+	}
+
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = "localhost"
+		handlers.Warning("invalid HOST. Defaults to `localhost`")
 	}
 
 	return &DBConfig{
-		Host:     os.Getenv("DB_HOST"),
+		Host:     host,
 		Port:     port,
 		User:     os.Getenv("DB_USER"),
 		Password: os.Getenv("DB_PASSWORD"),
